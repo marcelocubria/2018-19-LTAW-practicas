43f57f8ca0a54c49045a42b6a109cabdd59b896c
Marcelo Cubria Alonso <mcubria@beta21.aulas.gsyc.urjc.es>
2019-02-11 12:38:09 +0100

    servidor funcionando

--- Practica-1/web1/server.js
+++ Practica-1/web1/server.js
@@ -7,16 +7,29 @@ console.log("Arrancando servidor...");
 
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
-  var peticion = "." + q.pathname;
+  var peticion = q.pathname;
+  var tipofichero = peticion.split(".")[1];
+  if (peticion == "/") {
+    peticion = "./pagina1.html";
+    tipofichero = "html"
+  } else {
+    peticion = "." + peticion;
+  }
   console.log(peticion);
-  var tipofichero = peticion.split(".")[tipofichero.length-1];
   console.log(tipofichero);
+
   fs.readFile(peticion, function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
       return res.end("404 Not Found, Fichero no encontrado");
     }
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    mime = "text/html";
+    if (tipofichero == ("jpg" || "png")) {
+      mime = "image/" + tipofichero;
+    } else if (tipofichero == "css") {
+      mime = "text/css";
+    }
+    res.writeHead(200, {'Content-Type': mime});
     res.write(data);
     return res.end();
   });
