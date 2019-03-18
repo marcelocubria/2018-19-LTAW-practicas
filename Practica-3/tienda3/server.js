67af446fa7e662b5277056d5b888044e1213bb23
Marcelo Cubria Alonso <mcubria@beta13.aulas.gsyc.urjc.es>
2019-03-18 12:53:58 +0100

    avanzando

--- Practica-3/tienda3/server.js
+++ Practica-3/tienda3/server.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 console.log("Arrancando servidor...");
 
 
-http.createServer(function (req, res) {
+http.createServer((req, res) => {
   //-- Mostrar en la consola el recurso al que se accede
   var q = url.parse(req.url, true);
   var peticion = q.pathname;
@@ -19,33 +19,77 @@ http.createServer(function (req, res) {
   console.log(peticion);
   console.log(tipofichero);
 
-  //-- Leer las cookies
-  var cookie = req.headers.cookie;
-  console.log("Cookie: " + cookie)
+  switch (tipofichero) {
 
-  fs.readFile(peticion, function(err, data) {
+    case "html":
 
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found, Fichero no encontrado");
-    }
-    mime = "text/html";
-    if (tipofichero == ("jpg" || "png")) {
-      mime = "image/" + tipofichero;
-    } else if (tipofichero == "css") {
-      mime = "text/css";
-    }
+      if (q.pathname == "/login" && req.method === 'POST') {
 
-    //manejo cookie
-    if (!cookie) {
-      data.replace("Bienvenido,", "Bienvenido, usuario no registrado")
-    } else {
-      data.replace("Bienvenido,", "Bienvenido, " + cookie);
-    }
-    
-    res.writeHead(200, {'Content-Type': mime});
-    res.write(data);
-    return res.end();
-  });
+        req.on('data', chunk => {
+            //-- Leer los datos (convertir el buffer a cadena)
+            data = chunk.toString();
+
+            //-- Mostrar los datos en la consola del servidor
+            console.log("Datos recibidos: " + data)
+            res.statusCode = 200;
+         });
+         
+         req.on('end', ()=> {
+           //-- Generar el mensaje de respuesta
+           res.setHeader('Content-Type', 'text/html')
+           res.write(content);
+           res.end();
+         })
+         return
+     } else {
+
+        var cookie = req.headers.cookie;
+        console.log("Cookie: " + cookie);
+
+        fs.readFile(peticion, function(err, data) {
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          return res.end("404 Not Found, Fichero no encontrado");
+        }
+
+        htmlstring = data.toString();
+        //manejo cookie
+        if (!cookie) {
+          htmlstring2 = htmlstring.replace("Bienvenido,", "Bienvenido, usuario no registrado")
+        } else {
+          htmlstring2 = htmlstring.replace("Bienvenido,", "Bienvenido, " + cookie);
+        }
+        console.log(htmlstring2);
+
+        mime = "text/html";
+        res.writeHead(200, {'Content-Type': mime});
+        res.write(data);
+        return res.end();
+        });
+      }
+    break;
+
+    case "jpg":
+    case "png":
+    case "css":
+      fs.readFile(peticion, function(err, data) {
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          return res.end("404 Not Found, Fichero no encontrado");
+        }
+        mime = "text/html";
+        if (tipofichero == ("jpg" || "png")) {
+          mime = "image/" + tipofichero;
+        } else if (tipofichero == "css") {
+          mime = "text/css";
+        }
+        res.writeHead(200, {'Content-Type': mime});
+        res.write(data);
+        return res.end();
+      });
+    break;
+  }
+
   console.log("Peticion atendida");
+  console.log("");
 }).listen(8080);
